a60566715986f69ea6791ee74b5965412674e0db
R-J <robin.jurinka@gmx.net>
2014-01-08 16:55:51 +0100

    Update class.customsearch.plugin.php
    Hooked SearchModel, inserted lots of debug code
    Problem with SearchSql when joining results to discussions/comments

--- class.customsearch.plugin.php
+++ class.customsearch.plugin.php
@@ -109,6 +109,25 @@ class CustomSearchPlugin extends Gdn_Plugin {
          }
       }
    }
+
+/* test function */
+   public function DiscussionController_AfterCommentBody_Handler($Sender) {
+return;
+      $Post = $Sender->EventArguments['Comment']->Body;
+      if ($Post == '') {
+         $Post = $Sender->EventArguments['Discussion']->Body;
+         $DiscussionID = $Sender->EventArguments['Discussion']->DiscussionID;
+         $CommentID = 0;
+         $InsertUserID = $Sender->EventArguments['Discussion']->InsertUserID;
+         $DateInserted = $Sender->EventArguments['Discussion']->DateInserted;
+      } else {
+         $DiscussionID = 0;
+         $CommentID = $Sender->EventArguments['Comment']->CommentID;
+         $InsertUserID = $Sender->EventArguments['Comment']->InsertUserID;
+         $DateInserted = $Sender->EventArguments['Comment']->DateInserted;
+      }
+      $this->IndexText($Post, $DiscussionID, $CommentID, $InsertUserID, $DateInserted);
+   }
    
    private function IndexText($Post, $DiscussionID = 0, $CommentID = 0, $InsertUserID, $DateInserted) {
       $Px = Gdn::Database()->DatabasePrefix;
@@ -122,7 +141,7 @@ class CustomSearchPlugin extends Gdn_Plugin {
       
       // prepare wordlist
       // make post all lowercase
-      $WordList = mb_strtolower($Post);
+      $WordList = strtolower($Post);
       // strip html tags (don't know if this makes sense)
       // $WordList = strip_tags($WordList);
       // delete everything but letters, numbers and underscores
@@ -137,13 +156,12 @@ class CustomSearchPlugin extends Gdn_Plugin {
       $WordList = preg_replace('/\b(\w{0,'.(C('Plugins.CustomSearch.MinWordLength', '4') - 1).'}'.$StopWords.'|_)\b/u', ' ', $WordList);
       // compress multiple spaces to one
       $WordList = trim(preg_replace('/\s+/', ' ', $WordList));
+      // no keywords => nothing to do!
       if ($WordList == '') {
-         // no keywords => nothing to do!
          return;
       }
       // Format word list to use in sql insert command
       $WordList = "('".str_replace(' ', "'),('", $WordList)."')";
-      
       // insert all words into temp table
       $Sql->Insert('CSPostWord', array('Word'), 'VALUES '.$WordList);
       
@@ -159,5 +177,47 @@ class CustomSearchPlugin extends Gdn_Plugin {
       $CountQuery = "INSERT INTO {$Px}CSOccurance (DiscussionID, CommentID, InsertUserID, DateInserted, WordID, OccuranceCount) ";
       $CountQuery .= "SELECT {$DiscussionID}, {$CommentID}, {$InsertUserID}, '{$DateInserted}', WordID, COUNT(WordID) FROM {$Px}CSPostWord GROUP BY WordID";
       $Sql->Query($CountQuery);
+   }
+   
+   public function SearchModel_Search_Handler($Sender) {
+// $this->ReIndexAll();
+// return;   
+      $Px = Gdn::Database()->DatabasePrefix;
+
+      $Sender->Reset();
+      // $Sender->AddSearch("select 1 as `Relavence`    , 98 as `PrimaryID`    , 'Pfusch' as `Title`    , 'Tästmän ;)' as `Summary`    , 'HTML' `Format`    , 1 as `CategoryID`    , concat('/discussion/comment/', 98, '/#Comment_', c.CommentID) as `Url`    , '2013-04-01' as `DateInserted`    , 6 as `UserID` from    V20188_Comment c where c.CommentID = 98");
+      $Sql = <<<EOT
+SELECT
+     o.OccuranceCount AS `Relavence`    
+   , CASE WHEN o.CommentID = 0 THEN o.DiscussionID ELSE o.CommentID END AS `PrimaryID`
+   , d.Name AS `Title`
+   , CASE WHEN o.CommentID = 0 THEN d.Body ELSE c.Body END AS `Summary`
+   , CASE WHEN o.CommentID = 0 THEN d.Format ELSE c.Format END AS `Format`
+   , d.CategoryID AS `CategoryID`
+   , CASE WHEN o.CommentID = 0 THEN
+         CONCAT('/discussion/', o.DiscussionID)
+      ELSE
+         CONCAT('/discussion/comment/', o.CommentID, '/#Comment_', o.CommentID)
+      END AS `Url`
+   , o.DateInserted AS `DateInserted`
+   , o.InsertUserID AS `UserID`
+FROM
+   {$Px}CSOccurance o
+   LEFT JOIN {$Px}Discussion d ON o.DiscussionID = d.DiscussionID
+   LEFT JOIN {$Px}Comment c ON o.CommentID = c.CommentID
+WHERE
+   o.WordID IN (
+      SELECT WordID FROM {$Px}CSWordList
+      WHERE Word in ('newwordhere', 'admin')
+   )
+ORDER BY
+   o.OccuranceCount DESC
+   , o.DateInserted DESC
+EOT;
+      $Sender->AddSearch($Sql);
+//      decho($Sql);
+//      decho($Sender->EventArguments['Search']);
+//      decho($Sender);
+      
    }
 }
